febae72247ff630f2307a532f6f9e1f0e86eec15
miharbii <fradj.ibrahim@gmail.com>
2020-12-30 14:30:33 -0500

    works on toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
-
+import {MatIconModule} from '@angular/material/icon'
 @NgModule({
   declarations: [
     AppComponent,
     MainViewComponent,
-    ToolBarComponent
+    ToolBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
