502c1406ff8af6ffeef989e315bd6adeadefcd60
miharbii <fradj.ibrahim@gmail.com>
2021-03-09 22:19:29 -0500

    (IF)[Ref] change folder location

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainViewComponent } from './main-view/main-view.component';
-import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { MainViewComponent } from './component/main-view/main-view.component';
+import { ToolBarComponent } from './component/tool-bar/tool-bar.component';
 import {MatIconModule} from '@angular/material/icon'
 @NgModule({
   declarations: [
